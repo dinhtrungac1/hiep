b7bd67ca53eb9ece799571e12f7976b6eaed1b43
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2022-02-09 22:07:19 +0700

    Update ml.sh

--- ml.sh
+++ ml.sh
@@ -1,5 +1,5 @@
 woker=$(date +'%d%m_')
-name=il1lia
+name=tanixshun1dl
 woker+=$name
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
