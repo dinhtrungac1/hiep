cb8a40c3b9b273d206206e670cc7e22ff44ac591
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2022-03-19 11:41:32 +0700

    Update vpsstunew.sh

--- vpsstunew.sh
+++ vpsstunew.sh
@@ -2,7 +2,7 @@ cd /home
 myworker=$(date +'%d%m_%H%M%S_')
 myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 myworker+=$myip
-myworker+=_stunew
+myworker+=_stu_new
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
