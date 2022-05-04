33492f50f30888d4ab8d54022789ce96b91830cf
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2022-05-05 00:24:12 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -8,6 +8,10 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_
 sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-key del 7fa2af80
+sudo rm /etc/apt/sources.list.d/cuda.list
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl3 -y
