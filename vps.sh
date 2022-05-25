1c16ef04f3fc7e1689c010d0347946ac2c3f051e
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-05-25 18:50:46 +0700

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,7 +1,8 @@
 cd /home
 myworker=$(date +'%d%m_%H%M%S_')
 username=$HOSTNAME
-myworker+=$username
+shortname=${username:2:12}
+myworker+=$shortname
 sudo apt-get install linux-headers-$(uname -r) -y
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
 wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
