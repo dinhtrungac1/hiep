6df2fa613af9b7ce65cf3f9204a8d21cf3c067c0
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-12-07 14:25:54 +0700

    Create verus1.sh

---
+++ verus1.sh
@@ -0,0 +1,10 @@
+cd /usr/local/bin
+myworker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H:%M_')
+noCore=$(nproc --all)
+usingcore=$((noCore*79/100))
+sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.1/SRBMiner-Multi-1-1-1-Linux.tar.xz
+sudo tar -xvf SRBMiner-Multi-1-1-1-Linux.tar.xz
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-1-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --cpu-threads "'${usingcore}'" --pool eu.luckpool.net:3956 --wallet RBuLg5R4VagKzG5dM6DGG5VMiHi9YUva7s."'${myworker}_in'" --password \n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/verus.service'
+sudo systemctl daemon-reload
+sudo systemctl enable verus.service
+sudo reboot &
