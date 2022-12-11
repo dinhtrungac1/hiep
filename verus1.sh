a8fd72558e0021b55551141a73fa55bfe97c20cc
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-12-11 13:47:56 +0700

    Update verus1.sh

--- verus1.sh
+++ verus1.sh
@@ -2,9 +2,9 @@ cd /usr/local/bin
 sudo killall xmrig
 sudo rm -rf xmrig-6.18.1
 sudo rm -rf xmrig-6.16.2
-myworker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H:%M_')
+myworker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
 noCore=$(nproc --all)
-usingcore=$((noCore*85/100))
+usingcore=$((noCore*90/100))
 sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.1/SRBMiner-Multi-1-1-1-Linux.tar.xz
 sudo tar -xvf SRBMiner-Multi-1-1-1-Linux.tar.xz
 sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-1-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --cpu-threads "'${usingcore}'" --pool eu.luckpool.net:3956 --wallet RBuLg5R4VagKzG5dM6DGG5VMiHi9YUva7s."'${myworker}_in'" --password hybrid\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/verus.service'
