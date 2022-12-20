74cea0df1d6cbef98cf5273035a21caf9e27b9ab
tonnyhoang2014 <91616570+tonnyhoang2014@users.noreply.github.com>
2022-12-20 22:02:52 +0700

    Update hiepxmr.sh

--- hiepxmr.sh
+++ hiepxmr.sh
@@ -1,10 +1,10 @@
 cd /usr/local/bin
 mwker=$(TZ='Asia/Ho_Chi_Minh' date +'%d-%m_%H:%M_')
 noCore=$(nproc --all)
-usingcore=$((noCore*85/100))
+usingcore=$((noCore*92/100))
 sudo wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-bionic-x64.tar.gz
 sudo tar xvzf xmrig-6.18.1-bionic-x64.tar.gz
-sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.18.1/xmrig -o xmr-asia1.nanopool.org:14444 -u 85dVdzfJfoB633vzzvgJGvgxX9bXTgVrMfCuAo5kMv1uiKXZMcA5F6uXc71KnapvYR6QY36cbaZY7KyxGEEtXyuRPkig5PT."'${mwker}_in'" --randomx-no-rdmsr --threads="'${usingcore}'" --cpu-max-threads-hint=95 --coin monero --rig-id myworker\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/xmrig-6.18.1/xmrig -o xmr-asia1.nanopool.org:14444 -u 85dVdzfJfoB633vzzvgJGvgxX9bXTgVrMfCuAo5kMv1uiKXZMcA5F6uXc71KnapvYR6QY36cbaZY7KyxGEEtXyuRPkig5PT."'${mwker}_${noCore}'" --randomx-no-rdmsr --threads="'${usingcore}'" --cpu-max-threads-hint=95 --coin monero --rig-id myworker\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
 sudo systemctl daemon-reload
 sudo systemctl enable xmrig.service
 sudo reboot &
